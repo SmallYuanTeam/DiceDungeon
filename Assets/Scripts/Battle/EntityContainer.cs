040af2259624be89ffa3ddb1e16e8c2c4d7cf47d
まぅSmallYuan <alan6716s@gmail.com>
2024-02-28 00:06:03 +0900

    Create EntityContainer.cs

---
+++ Assets/Scripts/Battle/EntityContainer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class EntityContainer : MonoBehaviour 
+{
+    // Get All effect to the entity
+    public int Increace = 0; // 亢奮,增傷
+    public int Decrease = 0; // 減傷
+    public int Poison = 0; // 中毒
+    public int Dizzy = 0; // 暈眩
+    public int Burn = 0; // 燒傷
+    public int Bloodthirsty = 0; // 嗜血
+    public int Restrict = 0; // 限制
+    public int Paralysis = 0; // 麻痺
+    public int Freeze = 0; // 冰凍
+    public int Wound = 0; // 傷口
+    public int Rebellion = 0; // 盾反
+    public int Thorns = 0; // 荊棘
+    public int Psychedelic = 0; // 迷幻
+    public int Angry = 0; // 憤怒
+    public int Purify = 0; // 淨化
+    public int Pray = 0; // 祈禱
+    public int Curse = 0; // 詛咒
+    public int Utility = 0; // 實用
+
+}
